44c2bc78a64666d122a0b3983808f1228674f864
LukeMcCann <developer-lukemccann@outlook.com>
2023-03-18 20:36:44 +0000

    Add logging middleware

--- services/app/api/src/server.ts
+++ services/app/api/src/server.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import { ExpressApp } from './types/utils';
 import setupRoutes from './routes';
 import router from './router';
+import morgan from 'morgan';
 
 const configApp = () => {
 	const app = express() as ExpressApp;
 
+	// Configure App
 	app.Router = router;
 
+	// Start Middleware
+	app.use(morgan('dev'));
+
+	// Additional Setup
 	setupRoutes(app);
 
 	return app;
